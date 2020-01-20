837ea8fe949e00d8fa92af53a5d7f9303ec915da
qimingxia <qi.mingxia@huairuo.ai>
2020-01-20 15:20:46 +0800

    数据库操作

--- projects/models.py
+++ projects/models.py
@@ -19,7 +19,7 @@ class Projects(models.Model):
 	# unique来设置当前字段是否唯一，默认False
 	# max_length 设置限制字段的最大长度
 	id = models.AutoField(verbose_name='id主键', primary_key=True, help_text='id主键')
-	name= models.CharField(verbose_name='项目名称', help_text='项目名称', unique=True, max_length=200)
+	name = models.CharField(verbose_name='项目名称', help_text='项目名称', unique=True, max_length=200)
 	leader = models.CharField(verbose_name='项目负责人', help_text='项目负责人', max_length=50)
 	tester = models.CharField(verbose_name='测试人员', help_text='测试人员', max_length=50)
 	programmer = models.CharField(verbose_name='开发人员', help_text='开发人员', max_length=50)
