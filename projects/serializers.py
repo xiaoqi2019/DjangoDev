3dd9b7a087ef9142855ff6d0e57fe3e28eae5e8f
qimingxia <qi.mingxia@huairuo.ai>
2020-02-25 11:45:28 +0800

    DRF中generics拓展类代码优化

--- projects/serializers.py
+++ projects/serializers.py
@@ -24,63 +24,6 @@ def contain_keyword_project_name(value):
 	if "项目" not in value:
 		raise serializers.ValidationError("项目名命名未包含'项目'")
 
-# class ProjectSerializer(serializers.Serializer):
-# 	"""创建项目序列器
-# 	序列化器的作用：
-# 	a.序列化操作 --serialize = ProjectSerializer(instance=one_project)（转成json数据）
-# 	b.反序列化操作 --serialize.data(转成字典)
-# 	"""
-# 	# 1.定义的序列化器，需要继承Serializer或者子类
-# 	# 2. 定义的每一个类属性要与模型类中对应
-# 	# 3.label对应verbose_name
-# 	# 4.默认情况下，定义了哪些属性（序列化器字段），那么就会序列化输出哪些字段和哪些字段需要进行反序列化输入
-# 	# 如果不需要序列化输出，不定义即可
-# 	# 5.如果在定义的字段中，添加write_only=True,那么当前字段只能进行反序列化（数据校验），即只是输入校验不进行输出
-# 	# id = serializers.IntegerField(label='ID', write_only=True)
-# 	# 正常情况下id不需要数据校验，不需要输入，可能需要输出
-# 	# 6.如果在定义的字段中，添加read_only=True,那么和write_only=True相反，不进行输入校验，只进行输出
-# 	id = serializers.IntegerField(label='ID', read_only=True)
-# 	# 使用自定义校验器-校验顺序按照校验器列表顺序，每个都要校验（不管前面的失败还是成功）-为了查看所有出错的地方
-# 	name = serializers.CharField(label="项目名称", max_length=200, help_text="项目名称",
-# 	                             validators=[is_unique_project_name, contain_keyword_project_name])
-# 	# name = serializers.CharField(label='项目名称', max_length=200, help_text='项目名称',
-# 	#                              validators=[UniqueValidator(queryset=Projects.objects.all(), message='项目名不能重复')])
-# 	# 可以在序列化字段上，添加自定义抛错信息-error_messages选项（字典，校验选项的字符串为key，具体的报错信息为value）
-# 	leader = serializers.CharField(label='项目负责人', help_text='项目负责人', max_length=50,
-# 	                               error_messages={"max_length":"负责人名称的长度不能超过50个字符串"})
-# 	tester = serializers.CharField(label='测试人员', help_text='测试人员', max_length=50)
-# 	programmer = serializers.CharField(label='开发人员', help_text='开发人员', max_length=50)
-# 	publish_app = serializers.CharField(label='发布应用', help_text='发布应用', max_length=200)
-# 	desc = serializers.CharField(label='简要描述', help_text='简要描述', max_length=200, default='',
-# 	                             allow_null=True, allow_blank=True)
-#
-
-# 	def create(self, validated_data):
-# 		"""
-# 		创建项目
-# 		:param validated_data: 校验通过之后的项目数据
-# 		:return: 项目创建成功之后的模型类对象返回
-# 		"""
-# 		project = Projects.objects.create(**validated_data)
-# 		return project
-#
-# 	def update(self, instance, validated_data):
-# 		"""
-# 		更新项目
-# 		:param instance: 待更新的项目模型类对象
-# 		:param validated_data:
-# 		:return:项目更新成功之后的模型类对象返回
-# 		"""
-# 		# 3.更新项目--必须save()
-# 		instance.name = validated_data['name']
-# 		instance.leader = validated_data['leader']
-# 		instance.tester = validated_data['tester']
-# 		instance.programmer = validated_data['programmer']
-# 		instance.publish_app = validated_data['publish_app']
-# 		instance.desc = validated_data['desc']
-# 		instance.save()
-# 		return instance
-
 class ProjectModelSerializer(serializers.ModelSerializer):
 	# 定义Meta内部类，用于设置当前序列化器类的元数据信息
 	# 自带create()和update()方法
@@ -89,7 +32,7 @@ class ProjectModelSerializer(serializers.ModelSerializer):
 	# 子类模型序列化器类会创建子表字段，可以不用创建就可以显示；
 	# 如果是按照接口模型类指定的名字：interfaces，下面命名就按照指定的，没有就是interfaces_set
 	# 父类关联子类：
-	interfaces_set = serializers.StringRelatedField(many=True)
+	interfaces_set = serializers.StringRelatedField(many=True, read_only=True)
 
 	class Meta:
 		# 1.指定参照哪一个模型
@@ -107,7 +50,7 @@ class ProjectModelSerializer(serializers.ModelSerializer):
 		extra_kwargs = {
 			"name":{
 				"min_length":1,
-				"validators":[is_unique_project_name, contain_keyword_project_name]
+				# "validators":[is_unique_project_name, contain_keyword_project_name]
 			}
 		}
 	# 对单字段在序列化器内部进行检验,方法命名必须是：validate_字段名
@@ -136,12 +79,6 @@ class ProjectModelSerializer(serializers.ModelSerializer):
 			raise serializers.ValidationError("项目名称或者负责人中至少有一个包含‘小七’")
 		return attrs
 
-	# def create(self, validated_data):
-	# 	# 如果前端传给一个参数，模型字段里面没有，我们又需要校验，
-	# 	# 这时候需要重写模型序列化器的create()
-	# 	# 方法，将这个不需要入库的字段，传参时需要去掉，如下：
-	# 	validated_data.pop('email')
-	# 	super().create(validated_data)
 
 
 
--- projects/views.py
+++ projects/views.py
@@ -1,216 +1,23 @@
-import json
-
-from django.http import HttpResponse, JsonResponse, request, Http404
-
-# Create your views here.
-from django.views import View
-
-from django_filters.rest_framework import DjangoFilterBackend # 导入过滤
-from rest_framework import status
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import OrderingFilter
 from rest_framework.generics import GenericAPIView
-from rest_framework.pagination import PageNumberPagination
-from rest_framework.response import Response
-
-from interfaces.models import Interfaces
-from .models import Projects # 导入
-from .serializers import ProjectModelSerializer # 导入序列化类
-from rest_framework.views import APIView
+from .models import Projects
+from .serializers import ProjectModelSerializer
 from rest_framework import mixins
+from rest_framework import generics
 
 
 # 1.要实现过滤排序分页功能，需要继承DRF中的GenericAPIView
-class ProjectList(mixins.ListModelMixin,
-                  GenericAPIView):  # 相同url --get /projects  post /projects
-	# 往往要指定queryset查询集类属性
+# 先继承mixins的拓展类，需要放在GenericAPIView的前面继承
+class ProjectList(generics.ListCreateAPIView):
 	queryset = Projects.objects.all()
-	# 往往需要serializer_class指定，要是用的序列化器类
 	serializer_class = ProjectModelSerializer
-	# filter_backends指定过滤引擎和排序引擎
-	# 如果在全局配置变量中指定了，这里无需再次指认
-	# filter_backends = [DjangoFilterBackend, OrderingFilter] # 列表，可以些多个过滤引擎
-	# pagination_class = [PageNumberPagination] # 指定分页引擎
-	# filterset_fields指定可以过滤的字段，只能是模型类定义字段，而且是全匹配，不是模糊匹配
+	filter_backends = [DjangoFilterBackend, OrderingFilter]
 	filterset_fields = ['name', 'leader', 'tester']
-	# 指定排序字段，默认使用ordering作为key，指定字段名为value，默认升序，需要降序，字段前加减号
-	ordering_fields = ['id', 'name', 'leader'] # 指定排序字段
-
-	# def get(self, request):
-	# 	"""获取项目列表数据
-	# 	:param request
-	# 	:return:
-	# 	"""
-	# 	# 使用get_queryset方法获取查询集对象
-	# 	# project_qs = self.get_queryset() # 查询集对象中有100条项目模型对象
-	# 	project_qs = self.filter_queryset(self.get_queryset()) # 过滤之后可能只有20条
-	# 	page = self.paginate_queryset(project_qs)
-	# 	if page is not None:
-	# 		serializer = self.get_serializer(instance=page, many=True)
-	# 		return self.get_paginated_response(serializer.data)
-	# 	# name = request.query_params.get('name')
-	# 	# if name is not None:
-	# 	# 	project_qs = project_qs.filter(name_contains=name)
-	# 	# 返回多条数据（列表数据）那么many=True
-	# 	# 以后获取需要使用的序列化器类，不要用serializer_class,而使用get_serializer()
-	# 	serializer = self.get_serializer(instance=project_qs, many=True) #序列化传参需要instance接收参数（接收json格式）
-	# 	return Response(serializer.data, status=status.HTTP_200_OK) # safe=False返回只要是非字典都要添加
-
-	def get(self, request):
-		return self.list(request)
-
-	def post(self, request):
-		"""创建一个项目
-		:param request:
-		:return:"""
-		# q1:前端传参，以哪种形式传参-json
-		# q2:接收参数转化成python的基本类型&参数校验-校验过程比较复杂，当前省略
-		# json_data = request.body
-		# python_data = json.loads(json_data, encoding='utf-8') # 转成字典
-		# 继承DRF框架中APIView之后，request是DRF框架中的Request对象
-		#
-		serializer = ProjectModelSerializer(data=request.data) # 反序列化传参使用data接收参数（接收字典）
-		try:
-			serializer.is_valid(raise_exception=True)
-		except Exception:
-			return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-		# project = Projects.objects.create(**serialize.validated_data)
-		# 创建序列化对象时，给data传参
-		# 调用save(),会自动调用create()方法
-		serializer.save()
-		return Response(serializer.data, status=status.HTTP_201_CREATED)
+	ordering_fields = ['id', 'name', 'leader']
 
 
-class ProjectDetail(GenericAPIView):
-	# 往往要指定queryset查询集类属性
+class ProjectDetail(generics.RetrieveUpdateDestroyAPIView):
 	queryset = Projects.objects.all()
-	# 往往需要serializer_class指定，要是用的序列化器类
 	serializer_class = ProjectModelSerializer
 
-	def get(self, request, pk):
-		"""
-		获取指定项目的信息
-		:param request:
-		:param pk:
-		:return:
-		"""
-		one_project = self.get_object()
-		serialize = self.get_serializer(instance=one_project)
-		return Response(serialize.data, status=status.HTTP_200_OK)
-
-	def put(self, request, pk):
-		"""
-		更新指定项目
-		:param request:
-		:param pk:
-		:return:
-		"""
-		# 1.校验PK值
-		# 2.获取指定pk值的项目
-		one_project = self.get_object()
-		# 3.q1:前端传参，以哪种形式传参-json
-		# q2:接收参数转化成python的基本类型&参数校验-校验过程比较复杂，当前省略
-		# json_data = request.body
-		# python_data = json.loads(json_data, encoding='utf-8') # 转成字典
-		serializer = self.get_serializer(data=request.data, instance=one_project)
-		try:
-			serializer.is_valid(raise_exception=True)
-		except Exception:
-			raise Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-		# 更新项目创建序列化对象时，给instance和data同时传参
-		# 调用save(),会自动调用update()方法
-		serializer.save()
-		return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-	def delete(self, request, pk):
-		"""
-		删除指定项目
-		:param request:
-		:param pk:
-		:return:
-		"""
-		# 1.校验前端传递的pk值
-		# 2.获取指定pk的项目
-		one_project = self.get_object()
-		one_project.delete()
-		return Response(None, status=status.HTTP_204_NO_CONTENT)
-
-
-
-# class IndexView(View):
-# def get(self, request):
-	# 	# 查询方法
-	# 	# 获取一张表中的所有记录
-	# 	# 1：调用all()方法返回QuerySet对象
-	# 	# 2：QuerySet对象相当于是一个高性能的列表（惰性加载）
-	# 	# 3：支持列表的数字索引功能（返回的是一个模型类对象），切片操作（返回依然是模型类对象），不支持负索引
-	# 	# 4：QuerySet对象.first（）可以获取第一个元素，.last（）获取最后一个元素
-	# 	qs = Projects.objects.all()
-	# 	# 二：获取某个指定的记录，使用get()
-	# 	# 1：如果没有查询到记录会报错，如果查询到了多条也会报错，只有返回一条记录才不会报错
-	# 	# 2：返回的是模型类对象
-	# 	# 3：get方法：最好使用主键或者唯一键去查询，避免报错
-	# 	one_project = Projects.objects.get(id=1)
-	# 	# 三：获取多条记录，使用filter()
-	# 	# filter获取不到记录时会返回一个空列表，不会抛异常：一般会返回一个列表集：
-	# 	qs = Projects.objects.filter(id=1)
-	# 	return HttpResponse("查询项目成功")
-
-	# def get(self, request):
-	# 	# 删除方法
-	# 	# 方法1：先获取需要更新的模型对象
-	# 	# 然后删除
-	# 	# one_project = Projects.objects.get(id=9)
-	# 	# 也可使用pk--primary key
-	# 	one_project = Projects.objects.get(pk=8)
-	# 	one_project.delete()
-	# 	return HttpResponse("删除成功")
-
-	# def get(self, request):
-	# 	# 修改方法
-	# 	# 方法1：先获取需要更新的模型对象
-	# 	# 然后修改相关属性
-	# 	one_project = Projects.objects.get(id=9)
-	# 	one_project.tester = "测试02"
-	# 	one_project.save()
-	# 	return HttpResponse("修改成功")
-	#
-	# 	# 修改方法2：更新必须所有字段统一更新，用方法1即可：更新的方法2暂时不用；
-	# 	# Projects.objects.update()
-
-
-	# def get(self, request):
-	# 	# 创建操作
-	# 	# 方法1
-	# 	# one_project = Projects(name='国产飞机项目1', leader="XXX院士",tester="测试1",
-	# 	#                        programmer="龙的传人",publish_app="国产飞机应用",desc="项目简介")
-	# 	# one_project.save()
-	#
-	# 	# 方法2：
-	# 	one_project = Projects.objects.create(name='国产飞机项目4', leader="XXX院士",tester="测试1",
-	# 	                       programmer="龙的传人",publish_app="国产飞机应用",desc="项目简介")
-	# 	# 关联键方法1
-	# 	Interfaces.objects.create(name='国产飞机项目4登陆接口', tester='测试1',project=one_project)
-	# 	# 关联键方法2
-	# 	# Interfaces.objects.create(name='国产飞机项目4登陆接口',tester='测试1',project_id=one_project.id)
-	# 	return HttpResponse("项目创建成功!")
-
-	# def get(self, request):
-	# 	data = [
-	# 		{
-	# 		'project_name':'前程贷项目',
-	# 		'leader':'王小二',
-	# 		'app_name':'p2p平台应用'
-	# 	},
-	# 		{
-	# 			'project_name': '火星探索项目',
-	# 			'leader': '沃克',
-	# 			'app_name': '火星探索应用'
-	# 		},
-	# 		{
-	# 			'project_name': 'XX项目',
-	# 		 'leader': '乌尔',
-	# 		 'app_name': 'xxx应用'
-	# 		}
-	# 	]
-	# 	# 如果data非字典类型，需要加safe=True
-	# 	return JsonResponse(data, safe=True)
