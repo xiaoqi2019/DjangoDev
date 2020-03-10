e2c6e01eb8d2935d9dba2795e59d251898e28262
qimingxia <qi.mingxia@huairuo.ai>
2020-03-10 17:56:48 +0800

    实现判断用户名和邮箱是否存在接口

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -3,9 +3,11 @@ from django.urls import path
 from rest_framework_jwt.views import obtain_jwt_token
 from . import views
 
+
 urlpatterns = [
 	path('login/', obtain_jwt_token),
-	path('register/', views.RegisterView.as_view())
+	path('register/', views.RegisterView.as_view()),
+	path('<username>/count/', views.UsernameCountView.as_view()),
+	path('<email>/count/', views.EmailCountView.as_view()),
 ]
-
 
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from requests import Response
 from rest_framework.generics import CreateAPIView, RetrieveAPIView
-
+from rest_framework.views import APIView
+from django.contrib.auth.models import User
 from . import serializers
 
 
@@ -10,18 +12,43 @@ class RegisterView(CreateAPIView):
 	"""
 	serializer_class = serializers.RegisterSerializer
 
-class UserNameView(RetrieveAPIView):
+class UsernameCountView(APIView):
 	"""
-	验证用户名是否存在接口
+	验证用户名是否存在
 	"""
-	serializer_class = serializers.UsernameSerializer
+	def get(self, request, username):
+		"""
+		获取指定用户名数量
+		:param request:
+		:param username:
+		:return:
+		"""
+		count = User.objects.filter(username=username).count()
+		data = {
+			"username": username,
+			"count": count
+		}
+		return Response(data=data)
 
 
-class EmailView(RetrieveAPIView):
+class EmailCountView(APIView):
 	"""
-	验证邮箱是否存在接口
+	验证邮箱是否存在
 	"""
-	serializer_class = serializers.EmailSerializer
+
+	def get(self, request, email):
+		"""
+		获取指定用户名数量
+		:param request:
+		:param username:
+		:return:
+		"""
+		count = User.objects.filter(username=email).count()
+		data = {
+			"email": email,
+			"count": count
+		}
+		return Response(data=data)
 
 
 
