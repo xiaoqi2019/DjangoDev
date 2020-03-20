fabe52ee6c82860fdb02c3dffc94e945c71f1ced
qimingxia <qi.mingxia@huairuo.ai>
2020-03-20 16:58:24 +0800

    九大模块--获取某个项目接口详情列表不适用序列化的处理方式

--- utils/pagination.py
+++ utils/pagination.py
@@ -3,10 +3,10 @@ from rest_framework.pagination import PageNumberPagination
 
 class ManualPageNumberPagination(PageNumberPagination):
 	# 前端用户指定的页面key的名称
-	page_query_param = 'p'
+	page_query_param = 'page'
 	# 前端用户指定的每一条页数key的名称
-	page_size_query_param = 's'
+	page_size_query_param = 'size'
 	# 前端指定的每一页最多数据条数（当前不超过100条）
-	max_page_size = 100
+	max_page_size = 20
 	# 指定默认每页2条数据
-	page_size = 2
+	# page_size = 10
