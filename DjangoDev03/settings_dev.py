74d345d90e4b0de8bc7bc07b089cd08011c38dca
qimingxia <qi.mingxia@huairuo.ai>
2020-04-20 19:13:53 +0800

    前端代码打包压缩复制到后端代码

--- DjangoDev03/settings_dev.py
+++ DjangoDev03/settings_dev.py
@@ -26,7 +26,7 @@ sys.path.append(os.path.join(BASE_DIR, 'apps')) # 加到系统路径列表的尾
 SECRET_KEY = '0qgx5+#ej(s!y&#xz%0^7&g_onma($v%gentp_0ksgzaxo6jp7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # ALLOWED_HOSTS = ["外网ip", "localhost", "127.0.0.1"]
 # 设置可以用于访问项目的地址（ip,域名）
@@ -264,6 +264,6 @@ REPORTS_DIR = os.path.join(BASE_DIR, 'reports')
 SUITES_DIR = os.path.join(BASE_DIR, 'suites')
 
 # 创建STATIC_ROOT, 存放静态文件的目录
-STATIC_ROOT = os.path.join(BASE_DIR, "front_ends/static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 
